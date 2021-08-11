422186bddc224cb784552495745faa631671d5ca
jdaev <jdhn42@gmail.com>
2021-08-11 12:57:03 -0400

    task(price-calculator):add community oracle + deps PE-129

---
+++ src/utils/constants.ts
@@ -0,0 +1,11 @@
+/**
+ * Minimum ArDrive community tip from the Community Improvement Proposal Doc:
+ * https://arweave.net/Yop13NrLwqlm36P_FDCdMaTBwSlj0sdNGAC4FqfRUgo
+ *
+ * Voted on by the ArDrive community (vote #82):
+ * https://community.xyz/#-8A6RexFkpfWwuyVO98wzSFZh0d6VJuI-buTJvlwOJQ/votes/
+ */
+export const minArDriveCommunityARTip = 0.000_010_000_000;
+
+// ArDrive Profit Sharing Community Smart Contract
+export const communityTxId = '-8A6RexFkpfWwuyVO98wzSFZh0d6VJuI-buTJvlwOJQ';
