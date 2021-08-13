07a8847ee7adc661c625238893043a8d3dd46f8c
Derek Sonnenberg <derek@ardrive.io>
2021-08-12 22:07:02 -0500

    test(convert unit): Add more test cases PE-126

--- src/utils/convertBytes.test.ts
+++ src/utils/convertBytes.test.ts
@@ -1,36 +1,48 @@
 import { expect } from 'chai';
-import convertBytes from './convertBytes';
+import convertBytes from './convert_unit';
 
 describe('The convertBytes function', () => {
 	it('correctly converts bytes to KB', () => {
-		expect(convertBytes(1_000, 'KB')).to.equal(0.9765625);
+		expect(convertBytes(Math.pow(2, 10), 'B', 'KB')).to.equal(1);
 	});
 
 	it('correctly converts bytes to MB', () => {
-		expect(convertBytes(1_000_000, 'MB')).to.equal(0.953_674_316_406_25);
+		expect(convertBytes(Math.pow(2, 20), 'B', 'MB')).to.equal(1);
 	});
 
 	it('correctly converts bytes to GB', () => {
-		expect(convertBytes(1_000_000_000, 'GB')).to.equal(0.931_322_574_615_478_5);
+		expect(convertBytes(Math.pow(2, 30), 'B', 'GB')).to.equal(1);
 	});
 
 	it('correctly converts KB to bytes', () => {
-		expect(convertBytes(1, 'KB', true)).to.equal(1_024);
+		expect(convertBytes(1, 'KB', 'B')).to.equal(Math.pow(2, 10));
 	});
 
 	it('correctly converts MB to bytes', () => {
-		expect(convertBytes(1, 'MB', true)).to.equal(1_048_576);
+		expect(convertBytes(1, 'MB', 'B')).to.equal(Math.pow(2, 20));
 	});
 
 	it('correctly converts GB to bytes', () => {
-		expect(convertBytes(1, 'GB', true)).to.equal(1_073_741_824);
+		expect(convertBytes(1, 'GB', 'B')).to.equal(Math.pow(2, 30));
 	});
 
-	it('returns correctly with a negative byteCount', () => {
-		expect(convertBytes(-1_000, 'KB')).to.equal(-0.9765625);
+	it('correctly converts KB to MB', () => {
+		expect(convertBytes(Math.pow(2, 10), 'KB', 'MB')).to.equal(1);
 	});
 
-	it('returns correctly with a byteCount set to the maximum safe integer', () => {
-		expect(convertBytes(Number.MAX_SAFE_INTEGER, 'GB')).to.equal(8388607.999999999);
+	it('correctly converts GB to MB', () => {
+		expect(convertBytes(1, 'GB', 'MB')).to.equal(Math.pow(2, 10));
+	});
+
+	it('returns correctly with a negative byte count', () => {
+		expect(convertBytes(-Math.pow(2, 10), 'B', 'KB')).to.equal(-1);
+	});
+
+	it('returns correctly with a byte count set to 0', () => {
+		expect(convertBytes(0, 'B', 'MB')).to.equal(0);
+	});
+
+	it('returns correctly with a byte count set to the maximum safe integer', () => {
+		expect(convertBytes(Number.MAX_SAFE_INTEGER, 'B', 'GB')).to.equal(8_388_607.999_999_999);
 	});
 });
