57bfd001dd26fb6c91d8e41d20c161d6029b6cbf
Derek Sonnenberg <derek@ardrive.io>
2021-08-16 12:06:36 -0500

    test(calc unit boxes): Account for community tip in test cases PE-128

--- src/utils/calculate_unit_boxes.test.ts
+++ src/utils/calculate_unit_boxes.test.ts
@@ -4,33 +4,42 @@ import type { UnitBoxValues } from 'src/hooks/useCalculation';
 import { ARDataPriceRegressionEstimator } from './ar_data_price_regression_estimator';
 import { UnitBoxCalculator } from './calculate_unit_boxes';
 import type { ARDataPriceEstimator } from './ar_data_price_estimator';
+import type { ArDriveCommunityTip } from 'src/types';
 
 describe('UnitBoxCalculator class', () => {
 	let unitBoxCalculator: UnitBoxCalculator;
 	let stubbedPriceEstimator: SinonStubbedInstance<ARDataPriceEstimator>;
 
+	/** Expected result when fiat or ar fields change */
+	const expectedResult: UnitBoxValues = { bytes: 0.849609, fiat: 10, ar: 1 };
+
 	const fiatPerAr = 10;
-	const expectedResult: UnitBoxValues = { bytes: 1, fiat: 10, ar: 1 };
+	const arDriveCommunityTip: ArDriveCommunityTip = { minARCommunityTip: 0.000_010_000_000, tipPercentage: 0.15 };
 
 	before(() => {
 		stubbedPriceEstimator = stub(new ARDataPriceRegressionEstimator(true));
-		stubbedPriceEstimator.getByteCountForAR.callsFake(() => Promise.resolve(Math.pow(2, 10)));
+		stubbedPriceEstimator.getByteCountForAR.callsFake((v) => Promise.resolve(v * Math.pow(2, 10)));
 		stubbedPriceEstimator.getARPriceForByteCount.callsFake(() => Promise.resolve(1));
 		unitBoxCalculator = new UnitBoxCalculator(stubbedPriceEstimator);
 	});
 
 	it('calculateUnitBoxValues function returns the correct unitBoxes when using the bytes unit to calculate', async () => {
-		const actual = await unitBoxCalculator.calculateUnitBoxValues(1, 'bytes', fiatPerAr, 'KB');
-		expect(actual).to.deep.equal(expectedResult);
+		const actual = await unitBoxCalculator.calculateUnitBoxValues(1, 'bytes', fiatPerAr, arDriveCommunityTip, 'KB');
+
+		/**
+		 * Expected result when bytes field changes
+		 * Community tip is applied to the fiat/ar fields
+		 */
+		expect(actual).to.deep.equal({ bytes: 1, fiat: 8.5, ar: 0.85 });
 	});
 
 	it('calculateUnitBoxValues function returns the correct unitBoxes when using the fiat unit to calculate', async () => {
-		const actual = await unitBoxCalculator.calculateUnitBoxValues(10, 'fiat', fiatPerAr, 'KB');
+		const actual = await unitBoxCalculator.calculateUnitBoxValues(10, 'fiat', fiatPerAr, arDriveCommunityTip, 'KB');
 		expect(actual).to.deep.equal(expectedResult);
 	});
 
 	it('calculateUnitBoxValues function returns the correct unitBoxes when using the ar unit to calculate', async () => {
-		const actual = await unitBoxCalculator.calculateUnitBoxValues(1, 'ar', fiatPerAr, 'KB');
+		const actual = await unitBoxCalculator.calculateUnitBoxValues(1, 'ar', fiatPerAr, arDriveCommunityTip, 'KB');
 		expect(actual).to.deep.equal(expectedResult);
 	});
 });
