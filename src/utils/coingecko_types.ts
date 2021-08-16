6f3b6aa8f9950684933f276c09c0813081366572
Mati <matibat@gmail.com>
2021-08-16 15:49:43 -0300

    feat(fiat-oracle): Adds supported currencies to our static types, and makes some generalizations for coingecko_types.ts PE-137

--- src/utils/coingecko_types.ts
+++ src/utils/coingecko_types.ts
@@ -1,3 +1,5 @@
+import type { FiatID, TokenID } from './fiat_oracle_types';
+
 export interface CoinGeckoResponse {
 	/**
 	 * Whether the response status code returned a successful code (>200 && <300).
@@ -24,8 +26,84 @@ export interface CoinGeckoPriceRequestParams extends Record<string, string> {
 	vs_currencies: string;
 }
 
-export interface CoinGeckoARPriceResponseData {
-	arweave: {
-		usd: number;
-	};
-}
+export type CoinGeckoPriceResponseData = {
+	[token in TokenID]: CoinGeckoCoinValue;
+};
+
+type CoinGeckoCoinValue = {
+	[currency in FiatID]: number;
+};
+
+/* The below values were retrived from the CoinGecko API on Friday 13 of August.
+ * $ curl -X 'GET' \
+ *   'https://api.coingecko.com/api/v3/simple/supported_vs_currencies' \
+ *   -H 'accept: application/json'
+ */
+const coinGeckoSupportedVSCurrencies = [
+	'btc',
+	'eth',
+	'ltc',
+	'bch',
+	'bnb',
+	'eos',
+	'xrp',
+	'xlm',
+	'link',
+	'dot',
+	'yfi',
+	'usd',
+	'aed',
+	'ars',
+	'aud',
+	'bdt',
+	'bhd',
+	'bmd',
+	'brl',
+	'cad',
+	'chf',
+	'clp',
+	'cny',
+	'czk',
+	'dkk',
+	'eur',
+	'gbp',
+	'hkd',
+	'huf',
+	'idr',
+	'ils',
+	'inr',
+	'jpy',
+	'krw',
+	'kwd',
+	'lkr',
+	'mmk',
+	'mxn',
+	'myr',
+	'ngn',
+	'nok',
+	'nzd',
+	'php',
+	'pkr',
+	'pln',
+	'rub',
+	'sar',
+	'sek',
+	'sgd',
+	'thb',
+	'try',
+	'twd',
+	'uah',
+	'vef',
+	'vnd',
+	'zar',
+	'xdr',
+	'xag',
+	'xau',
+	'bits',
+	'sats'
+] as const;
+
+const coinGeckoSupportedTokens = ['arweave'] as const;
+
+export type CoinGeckoTokenId = typeof coinGeckoSupportedTokens[number];
+export type CoinGeckoVSCurrency = typeof coinGeckoSupportedVSCurrencies[number];
