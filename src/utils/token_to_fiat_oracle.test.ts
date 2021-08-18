df8e7502a249ac53e2f7257fade97f431b169f0b
Mati <matibat@gmail.com>
2021-08-18 20:51:52 -0300

    test(fiat-oracle): Fixes and cleans the tests for the new code PE-137

--- src/utils/token_to_fiat_oracle.test.ts
+++ src/utils/token_to_fiat_oracle.test.ts
@@ -1,65 +1,43 @@
 import { expect } from 'chai';
 import { stub } from 'sinon';
-import type { CoinGeckoPriceResponseData } from './coingecko_types';
-import type { FiatID, TokenID } from './fiat_oracle_types';
-import { TokenFiatPair } from './token_fiat_price';
-import { TokenToFiatOracle } from './token_to_fiat_oracle';
+import { TokenFiatRate } from './token_fiat_price';
+import { CachingTokenToFiatOracle, TokenToFiatOracle } from './token_to_fiat_oracle';
 
-const token: TokenID = 'ARWEAVE';
-const fiat: FiatID = 'USD';
-const testingPair = new TokenFiatPair(fiat, token);
+type ArweaveID = 'ARWEAVE';
+type UsdID = 'USD';
+
+const token: ArweaveID = 'ARWEAVE';
+const fiat: UsdID = 'USD';
 const testingCacheLifespan = 1000 * 2; // 2 sec
-const halfOfTheTestingLifespan = testingCacheLifespan / 2;
 
 const examplePriceValue = 15.05;
-const coinGeckoPriceResponseForPair: CoinGeckoPriceResponseData = {
-	[token]: {
-		[fiat]: examplePriceValue
-	}
-};
-const mockResponse = new Response(
-	new Blob([JSON.stringify(coinGeckoPriceResponseForPair)], { type: 'application/json' })
-);
-
-describe('The fiat oracle', () => {
-	const stubbedFetch = stub({ fetch });
-	let oracle: TokenToFiatOracle;
+describe('The TokenToFiatOracle class', () => {
+	const stubbedOracle = stub(new TokenToFiatOracle(fiat, token));
+	let oracle: CachingTokenToFiatOracle<UsdID, ArweaveID>;
 
 	before(() => {
-		oracle = new TokenToFiatOracle([fiat], [token], testingCacheLifespan, stubbedFetch.fetch);
-		stubbedFetch.fetch.returns(new Promise((res) => res(mockResponse)));
+		oracle = new CachingTokenToFiatOracle(fiat, token, testingCacheLifespan, stubbedOracle);
+		stubbedOracle.getPriceForFiatTokenPair.callsFake(
+			async () => new TokenFiatRate<UsdID, ArweaveID>(fiat, token, examplePriceValue)
+		);
 	});
 
-	describe('Cache lifespan', () => {
-		it('No data fetched if just initialized', () => {
-			expect(stubbedFetch.fetch.callCount).to.equal(0);
-		});
+	it('Does not fetch data when just initialized', () => {
+		expect(stubbedOracle.getPriceForFiatTokenPair.callCount).to.equal(0);
+	});
 
-		it('One single fetch call when the cache is still valid', async (done) => {
-			// the first milliseconds since just created
-			await oracle.getPriceForFiatTokenPair(testingPair);
-			await oracle.getPriceForFiatTokenPair(testingPair);
-			setTimeout(async () => {
-				// half time passed
-				await oracle.getPriceForFiatTokenPair(testingPair);
-				await oracle.getPriceForFiatTokenPair(testingPair);
-				expect(stubbedFetch.fetch.callCount).to.equal(1);
-				done();
-			}, halfOfTheTestingLifespan);
-		});
+	it('Performs a fetch when getPriceForFiatTokenPair() is first called', async () => {
+		await oracle.getPriceForFiatTokenPair();
+		expect(stubbedOracle.getPriceForFiatTokenPair.callCount).to.equal(1);
+	});
 
-		it('There appears a second fetch call after the cache expires', async (done) => {
-			// first milliseconds after half time just passed
-			await oracle.getPriceForFiatTokenPair(testingPair);
-			await oracle.getPriceForFiatTokenPair(testingPair);
-			expect(stubbedFetch.fetch.callCount).to.equal(1);
-			setTimeout(async () => {
-				// first milliseconds after the cache has expired
-				await oracle.getPriceForFiatTokenPair(testingPair);
-				await oracle.getPriceForFiatTokenPair(testingPair);
-				expect(stubbedFetch.fetch.callCount).to.equal(2);
+	it('Performs a second fetch when getPriceForFiatTokenPair() is called after the timeout expires', function (done) {
+		this.timeout(testingCacheLifespan + 100);
+		setTimeout(() => {
+			oracle.getPriceForFiatTokenPair().then(() => {
+				expect(stubbedOracle.getPriceForFiatTokenPair.callCount).to.equal(2);
 				done();
-			}, halfOfTheTestingLifespan);
-		});
+			});
+		}, testingCacheLifespan);
 	});
 });
