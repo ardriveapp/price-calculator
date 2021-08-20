a929199ef305c1a5f3f5dddad53f82f28d4d0a0c
Mati <matibat@gmail.com>
2021-08-20 13:05:51 -0300

    feat(fiat-oracle): Fixes wrong export on utils PE-137

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,3 +1,3 @@
-export { ARDataPriceEstimator } from './ar_data_price_estimator';
+export type { ARDataPriceEstimator } from './ar_data_price_estimator';
 export { CachingTokenToFiatOracle } from './caching_token_to_fiat_oracle';
 export { ARDataPriceRegressionEstimator } from './ar_data_price_regression_estimator';
