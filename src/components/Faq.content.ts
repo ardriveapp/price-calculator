5c7870eef62748751e34801540e2173312773c9d
Mati <matibat@gmail.com>
2021-08-23 12:44:40 -0300

    feat(faq): Replases the 'Lorem ipsum' for the actual FAQs answers PE-174

--- src/components/Faq.content.ts
+++ src/components/Faq.content.ts
@@ -1,22 +1,22 @@
 export const faqQuestionsAnswers = [
 	{
 		question: 'How much storage do I need?',
-		answer: 'Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit...'
+		answer: 'A little bit of money can go a long way in data storage. For $1 USD you can purchase storage for approximately 1000 documents, 55 photos, or a 30 second video. Files of the same type (docs, jpegs, or mp3) will vary in sizes, but some rules of thumb are: 1 document 0.32 MB (320kb); 1 HD Photo 2.5 mb; 3.5 minute Song (3.5 MB); 1 minute HD video 100 MB.'
 	},
 	{
 		question: 'How are fees calculated?',
-		answer: 'Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit...'
+		answer: 'The file size is used to determine the base fee to upload data to the network. The price of the base fee is set by the Arweave Network code itself.   The Price Calculator includes this base fee and adds the ArDrive Community Tip percentage on top.  Learn more about ArDrive Fees.'
 	},
 	{
 		question: 'What is AR?',
-		answer: 'Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit...'
+		answer: 'AR is the name of the cryptocurrency used to upload data onto the Arweave network. Similar to having to pay USD when in the United States, many crypto blockchains have their own currency that you are required to use when you are in their ‘land’.  Therefore, ArDrive users must have a wallet with AR tokens in it to pay for data they upload. Learn more about Arweave tokens.'
 	},
 	{
 		question: 'Is the price of data consistent?',
-		answer: 'Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit...'
+		answer: 'Not exactly.  The Arweave network protocol is always adjusting the data price to maintain sustainable, perpetual storage.  The data price is lowered as the network increases its overall capacity.  This can result in a fluctuating data price, with a 30 day average of $6 per GB. However, for most files these fluctuations would only increase or decrease costs by fractions of a cent.'
 	},
 	{
 		question: 'How do I know how big a file is?',
-		answer: 'Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit...'
+		answer: 'In ArDrive, before any files are uploaded to the network, we calculate their size and estimate the price to upload.  Before it is paid for and stored permanently, you must approve the upload.  This ensures you are only uploading the right data for the right price.'
 	}
 ];
