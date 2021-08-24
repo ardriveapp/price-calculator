89670c6a4b276751a718d13a720cb1cdb039a22f
Derek Sonnenberg <derek@ardrive.io>
2021-08-24 11:47:43 -0500

    feat(text box): Add aria-labels to text inputs PE-144

--- src/components/TextBox.tsx
+++ src/components/TextBox.tsx
@@ -3,6 +3,7 @@ import { TextBoxContainer, TextBoxInput } from './TextBox.style';
 import CurrentUnit from './CurrentUnit';
 import type { UnitBoxes } from '../types';
 import { useStateValue } from '../state/state';
+import { getSpokenWord } from '../utils/get_spoken_word';
 
 interface TextBoxProps {
 	input?: number;
@@ -13,7 +14,12 @@ export default function TextBox({ input, field }: TextBoxProps): JSX.Element {
 	const [{ unitBoxes }] = useStateValue();
 	return (
 		<TextBoxContainer>
-			<TextBoxInput type="number" name="textbox" value={input} />
+			<TextBoxInput
+				type="number"
+				name="textbox"
+				value={input}
+				aria-label={`${getSpokenWord(unitBoxes[field].currUnit)} input field`}
+			/>
 			<CurrentUnit
 				onChange={(val) => console.log(val)}
 				units={unitBoxes[field].units}
