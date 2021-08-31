6dbd08d58f7490b17f9443e91146aed7174e5307
Derek Sonnenberg <derek@ardrive.io>
2021-08-31 14:42:45 -0500

    style(text box): Add border on focus instead of outline PE-209

--- src/components/TextBox.style.tsx
+++ src/components/TextBox.style.tsx
@@ -4,6 +4,7 @@ export const TextBoxInput = styled.input`
 	width: 65%;
 	height: 50%;
 	padding: 0 1rem;
+	outline: none;
 
 	@media (min-width: 800px) {
 		padding: 0 2rem;
@@ -19,6 +20,15 @@ export const TextBoxContainer = styled.div`
 	background-color: #fafafa;
 	box-shadow: 0 0 10px 5px rgba(213, 213, 213, 0.5);
 	font-family: 'Wavehaus-Bold';
+
+	border: 1px;
+	border-color: transparent;
+	border-style: solid;
+
+	:focus-within {
+		border-color: #4c4c4c;
+		box-shadow: 0 0 15px 10px rgba(20, 46, 110, 0.1);
+	}
 
 	@media (min-width: 800px) {
 		height: 65px;
