67c26c92dcb0e330f7d85a8f7a78322c13027eb9
Mati <matibat@gmail.com>
2023-09-25 13:03:20 -0300

    feat(faq): updates the FAQs PE-4360

--- src/components/Faq.content.tsx
+++ src/components/Faq.content.tsx
@@ -1,17 +1,22 @@
 import * as React from 'react';
-export const faqQuestionsAnswers = [
+export const faqQuestionsAnswers: Array<{
+	question: string;
+	answer: React.JSX.Element;
+}> = [
 	{
 		question: 'How much storage do I need?',
 		answer: (
 			<>
 				<p>
-					A little bit of money can go a long way in data storage. As you can see with the price calculator
-					for a small amount of USD you can purchase storage for hundreds of documents or dozens of photos or
-					songs.
+					A little bit of money can go a long way in data storage. As
+					you can see, a small amount of USD can purchase storage for
+					thousands of documents or hundreds of photos or songs.
 				</p>
 				<p>
-					Files of the same type (docs, jpegs, or mp3) will vary in sizes, but some rules of thumb are: 1
-					document 0.31 MB (320KB); 1 HD Photo 2.5 MB; 3.5 minute Song 3.5 MB; 1 minute HD video 100 MB.
+					Files of the same type (docs, jpegs, or mp3) will vary in
+					sizes, but in general: 1 document 0.31 MB (320KB); 1 HD
+					Photo 2.5 MB; 3.5 minute Song 3.5 MB; 1 minute HD video 100
+					MB.
 				</p>
 			</>
 		)
@@ -21,12 +26,13 @@ export const faqQuestionsAnswers = [
 		answer: (
 			<>
 				<p>
-					The file size is used to determine the base fee to upload data to the network. The price of the base
-					fee is set by the Arweave Network code itself. The Price Calculator includes this base fee and adds
-					the ArDrive Community Tip percentage on top. Learn more about{' '}
-					<a href="https://ardrive.atlassian.net/wiki/spaces/help/pages/86376465">ArDrive Fees</a>.
+					The file size determines the fee to upload data to the
+					network. The larger the file the higher the price will be.
 				</p>
-				<p> Note: All file sizes are represented using binary units of measurement (i.e. 1 MB = 1024 KB).</p>
+				<p>
+					Note: All file sizes are represented using binary units of
+					measurement (i.e. 1 MB = 1024 KB).
+				</p>
 			</>
 		)
 	},
@@ -35,13 +41,37 @@ export const faqQuestionsAnswers = [
 		answer: (
 			<>
 				<p>
-					AR is the name of the cryptocurrency used to upload data onto the Arweave network. Similar to having
-					to pay USD when in the United States, many crypto blockchains have their own currency that you are
-					required to use when you are in their ‘land’.{' '}
+					AR is the name of the cryptocurrency used to upload data
+					onto the Arweave network. Similar to having to pay USD when
+					in the United States, many blockchains have their own
+					currency that you are required to use when you are in their
+					‘land’. You have the option of paying for uploads using AR
+					or you can use your credit card to purchase Credits to
+					upload through ArDrive.
 				</p>
 				<p>
-					Therefore, ArDrive users must have a wallet with AR tokens in it to pay for data they upload. Learn
-					more about <a href="https://ardrive.io/what-is-arweave/">Arweave tokens</a>.
+					<span>Learn more about </span>
+					<a href="https://ardrive.io/what-is-arweave/">
+						Arweave tokens
+					</a>
+					.
+				</p>
+			</>
+		)
+	},
+	{
+		question: 'What are Credits?',
+		answer: (
+			<>
+				<p>
+					Credits offers users the ability to pay via credit card
+					instead of using the AR Token. Credits represent a 1:1 value
+					with the AR Token, but are used solely to pay for uploads
+					with ArDrive.
+				</p>
+				<p>
+					<span>Learn more about </span>
+					<a href="https://ardrive.io/turbo/infographic/">Credits</a>.
 				</p>
 			</>
 		)
@@ -51,13 +81,15 @@ export const faqQuestionsAnswers = [
 		answer: (
 			<>
 				<p>
-					Not exactly. The Arweave network protocol is always adjusting the data price to maintain
-					sustainable, perpetual storage. The data price is lowered as the network increases its overall
-					capacity.
+					Not exactly. The Arweave network protocol is always
+					adjusting the data price to maintain sustainable, perpetual
+					storage. The data price is lowered as the network increases
+					its overall capacity.
 				</p>
 				<p>
-					This can result in a fluctuating data price. However, for most files these fluctuations would only
-					increase or decrease costs by fractions of a cent.
+					This can result in a fluctuating data price. However, for
+					most files these fluctuations would only increase or
+					decrease costs by fractions of a cent.
 				</p>
 			</>
 		)
@@ -66,9 +98,20 @@ export const faqQuestionsAnswers = [
 		question: 'How do I know how big a file is?',
 		answer: (
 			<p>
-				In ArDrive, before any files are uploaded to the network, we calculate their size and estimate the price
-				to upload. Before it is paid for and stored permanently, you must approve the upload. This ensures you
-				are only uploading the right data for the right price.
+				In ArDrive, before any files are uploaded to the network, we
+				calculate their size and estimate the price to upload. Before it
+				is paid for and stored permanently, you must approve the upload.
+				This ensures you are only uploading the right data for the right
+				price.
+			</p>
+		)
+	},
+	{
+		question: 'How do I use a credit card?',
+		answer: (
+			<p>
+				You can purchase Credits with your credit card in the web app
+				from the profile menu.
 			</p>
 		)
 	}
