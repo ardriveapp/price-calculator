1817e1cada6fcf2ed5fe9a6d23e4dbdb6154e60b
dtfiedler <dylan@ar.io>
2023-11-16 10:27:46 -0600

    chore(polyfills): add vite polyfills to avoid issue with crypto
    Arbundles does not export polyfills by default, so we need to include one. The turbo-sdk could provide them, so we can look into the best resolution so consuming teams do not have to provide them.

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,10 @@ import react from '@vitejs/plugin-react';
 import viteTsconfigPaths from 'vite-tsconfig-paths';
 import svgrPlugin from 'vite-plugin-svgr';
 import packageJson from './package.json';
-import polyfillNode from 'rollup-plugin-polyfill-node';
+import nodePolyfills from 'vite-plugin-node-stdlib-browser';
 
 export default defineConfig({
-	plugins: [react(), viteTsconfigPaths(), svgrPlugin(), polyfillNode()],
+	plugins: [react(), viteTsconfigPaths(), svgrPlugin(), nodePolyfills()],
 	define: {
 		'import.meta.env.PACKAGE_VERSION': JSON.stringify(packageJson.version)
 	}
