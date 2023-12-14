69b47f28ef6952ff233f2aec3140577695c6e107
dtfiedler <dylan@ar.io>
2023-12-13 19:44:10 -0800

    chore(turbo): bump turbo-sdk and remove polyfill
    The turbo-sdk pushed a fix that removes the need for special polyfills.

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,9 @@ import react from '@vitejs/plugin-react';
 import viteTsconfigPaths from 'vite-tsconfig-paths';
 import svgrPlugin from 'vite-plugin-svgr';
 import packageJson from './package.json';
-import nodePolyfills from 'vite-plugin-node-stdlib-browser';
 
 export default defineConfig({
-	plugins: [react(), viteTsconfigPaths(), svgrPlugin(), nodePolyfills()],
+	plugins: [react(), viteTsconfigPaths(), svgrPlugin()],
 	define: {
 		'import.meta.env.PACKAGE_VERSION': JSON.stringify(packageJson.version)
 	}
